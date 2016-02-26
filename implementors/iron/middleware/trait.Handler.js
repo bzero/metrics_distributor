18522f9eedc7fddc58c21f32e99ef98dacefd228
Dirk Gadsden <dirk@esherido.com>
2016-02-26 22:00:28 +0000

    Auto-rebuild pages at 75cfcf6

---
+++ implementors/iron/middleware/trait.Handler.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['iron'] = [];implementors['router'] = ["impl <a class='trait' href='iron/middleware/trait.Handler.html' title='iron::middleware::Handler'>Handler</a> for <a class='struct' href='router/struct.Router.html' title='router::Router'>Router</a>",];implementors['metrics_distributor'] = ["impl <a class='trait' href='iron/middleware/trait.Handler.html' title='iron::middleware::Handler'>Handler</a> for <a class='struct' href='metrics_distributor/collectors/http/struct.LogDrainHandler.html' title='metrics_distributor::collectors::http::LogDrainHandler'>LogDrainHandler</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
