5e2baf3d4644862d737eab1779ea33e87841fcd7
Dirk Gadsden <dirk@esherido.com>
2016-02-29 20:54:57 +0000

    Auto-rebuild pages at 895c372

--- implementors/core/str/pattern/trait.ReverseSearcher.js
+++ implementors/core/str/pattern/trait.ReverseSearcher.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['regex_syntax'] = [];implementors['libc'] = [];implementors['openssl'] = [];implementors['hyper'] = [];implementors['iron'] = [];
+implementors['libc'] = [];implementors['regex_syntax'] = [];implementors['openssl'] = [];implementors['hyper'] = [];implementors['iron'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
